678e6138ee8ff2bc09e0780b1faa0ce03d6b942b
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-01-03 21:31:18 +0100

    completed styling of contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -65,14 +65,14 @@ export default function Contact () {
                                 <Col sm={6} className="px-1">
                                     <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
                                 </Col>
-                                <Col>
+                                <Col className="px-1">
                                     <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                                     <button type="submit"><span>{buttonText}</span></button>
                                 </Col>
                                 {
                                     status.message &&
-                                    <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                                    <Col sm={12} className="px-1 mt-3">
+                                        <p className={status.success === false ? "danger" : "success"} id="status">{status.message}</p>
                                     </Col>
                                 }
                             </Row>
