2be5a529443d40dda1c1c08940d90e97407f1ca1
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-01-04 14:24:58 +0100

    completed footer build

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+import logo from "../assets/img/header-img.svg"
+import navIcon1 from '../assets/img/github.svg'
+import navIcon2 from '../assets/img/nav-icon1.svg'
+import navIcon3 from '../assets/img/twitter.svg'
+
 export default function Footer () {
-    
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-items-center">
+                    <Col sm={6}>
+                        <img src={logo} alt="logo" />
+                    </Col>
+                    <Col sm={6} className="text-center text-sm-end">
+                        <div className="social-icon">
+                          <a href="https://www.github.com/ehidiamhen/" target='_blank'><img src={navIcon1} alt="nav1"/></a>
+                          <a href="https://www.linkedin.com/in/ehidiamhen/" target='_blank'><img src={navIcon2} alt="nav2"/></a>
+                          <a href="https://twitter.com/ehis_eleazar" target='_blank'><img src={navIcon3} alt="nav3"/></a>
+                        </div>
+                        <p>Copyright {year}. By Ehidiamhen</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
