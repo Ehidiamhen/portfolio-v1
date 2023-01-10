5cd2cbdb99d5e1bacaf7edbb91c3a40a8bcce9cc
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-01-10 22:39:37 +0100

    updated hero text (about me)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import headerImg from "../assets/img/header-img.svg"
 export default function Hero () {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Computer Programmer", "Software Developer", "Frontend Engineer" ];
+    const toRotate = ["Computer Programmer", "Software Engineer", "Frontend Developer" ];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100)
     const period = 2000;
@@ -47,7 +47,7 @@ export default function Hero () {
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1 className="txt-rotate">{`Hi I'm Ehiz, a `}<span className="wrap">{text}</span></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nemo eaque nulla iusto nostrum hic! Aut facere deleniti molestias, fugiat nihil aspernatur accusamus odit magni consectetur, provident numquam labore ex?</p>
+                        <p>I am a frontend developer specializing in creating modern, dynamic and responsive websites and applications. I task myself with constantly learning and staying up to date on the latest web development technologies. With a strong knowledge of core frontend languages and frameworks, I am committed to building functional and elegant user interfaces.</p>
                         <a href="#connect">Let's Connect <ArrowRightCircle size={25} /></a>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
