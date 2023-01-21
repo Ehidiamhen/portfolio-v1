b4e6e21d92f3757537abce1138cecf9e3801c0b2
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2023-01-21 23:14:03 +0100

    replaced hero file and updated navbar styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,49 +4,14 @@ import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
 
 export default function Hero () {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Computer Programmer", "Software Engineer", "Frontend Developer" ];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100)
-    const period = 2000;
-
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta)
-
-        return () => { clearInterval(ticker) };
-    }, [text]);
-
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if(isDeleting) {
-            setDelta(prevDelta => prevDelta/2)
-        };
-
-        if(!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period);
-        } else if(isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(500);
-        };
-    };
-
     return (
         <section className="hero" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
-                        <h1 className="txt-rotate">{`Hi I’m Ehiz, a `}<span className="wrap">{text}</span></h1>
+                        <h1 className="txt-rotate">Hi! I’m Aigbiluese Ehidiamhen</h1>
+                        {/* <h1 className="txt-rotate">{`Hi I’m Ehiz, a `}<span className="wrap">{text}</span></h1> */}
                         <p>I am a frontend developer specializing in creating modern, dynamic and responsive websites and applications. I task myself with constantly learning and staying up to date on the latest web development technologies. With a strong knowledge of core frontend languages and frameworks, I am committed to building functional and elegant user interfaces.</p>
                         <a href="#connect">Let’s Connect <ArrowRightCircle size={25} /></a>
                     </Col>
