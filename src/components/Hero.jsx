d732adc4ba89ef311f2caf5f80faa70dac1df040
Ehidiamhen Aigbiluese <109830337+Ehidiamhen@users.noreply.github.com>
2024-11-23 19:30:46 +0100

    chore: update resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero () {
                         <h1 className="txt-rotate">Hi! I’m Aigbiluese Ehidiamhen</h1>
                         {/* <h1 className="txt-rotate">{`Hi I’m Ehiz, a `}<span className="wrap">{text}</span></h1> */}
                         <p>I am a frontend developer specializing in creating modern, dynamic and responsive websites and applications. I task myself with constantly learning and staying up to date on the latest web development technologies. With a strong knowledge of core frontend languages and frameworks, I am committed to building functional and elegant user interfaces.</p>
-                        <a href="https://drive.google.com/file/d/1mrey29335BVPgl3R0uIyyDi2IcNUGkbq/view?usp=sharing" target='_blank'>My Resume <ArrowRightCircle size={25} /></a>
+                        <a href="https://drive.google.com/file/d/1knK5WgIVyxF5NhHD6Wx0P3IDDSSECymJ/view?usp=sharing" target='_blank'>My Resume <ArrowRightCircle size={25} /></a>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="header" />
